e9901a5c0fdfee786486bd993e2dff48ffbd17db
michaelPotter <mpotter.dev@gmail.com>
2024-07-30 13:55:45 -0700

    bump octoprint version

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -10,6 +10,6 @@ export KUBECONFIG=~/.kube/personal
 pod=$(kubectl get pods | egrep '^printer' | acut 1)
 
 d=octoprint_pushover_mlpotter
-copy_cmd="kubectl cp octoprint_pushover/__init__.py $pod:/octoprint/plugins/lib/python3.8/site-packages/octoprint_pushover"
+copy_cmd="kubectl cp octoprint_pushover/__init__.py $pod:/octoprint/plugins/lib/python3.10/site-packages/octoprint_pushover"
 # copy_cmd="cp -r $d/* /home/mlpotter/tmp/octoprint_plugin_test/venv/lib/python3.10/site-packages/octoprint_pushover"
 fd __init__.py | entr $copy_cmd
